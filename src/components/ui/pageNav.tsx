99b2b87528eedd3215dd05695d563b42442d614e
dithnim <nimsaradineth63@gmail.com>
2025-07-08 15:55:06 +0530

    Enhance PageNav component layout by adjusting icon and user info sizes, and improving text alignment and styling for better readability.

--- src/components/ui/pageNav.tsx
+++ src/components/ui/pageNav.tsx
@@ -11,7 +11,7 @@ const pageNav: React.FC<PageNavProps> = ({ name, position, title }) => {
     <nav className="flex items-center justify-between pe-6 bg-[#F8F6F7] rounded-t-lg border-b-2 border-[#E0E0E0]">
       {/* Left: Menu icon and page title */}
       <div className="flex">
-        <div className="bg-[#ee3e41] w-20 h-20 flex items-center justify-center me-6">
+        <div className="bg-[#ee3e41] w-25 h-25 flex items-center justify-center me-6">
           <i className="bx bx-menu font-bold text-3xl text-white"></i>{" "}
         </div>
         <div className="flex items-center justify-between">
@@ -20,7 +20,7 @@ const pageNav: React.FC<PageNavProps> = ({ name, position, title }) => {
       </div>
       {/* Right: User info */}
       <div className="flex items-center">
-        <div className="w-10 h-10 rounded-full bg-[#E0E0E0] flex items-center justify-center mr-3">
+        <div className="w-12 h-12 rounded-full bg-[#E0E0E0] flex items-center justify-center mr-3">
           <svg
             width="28"
             height="28"
@@ -32,11 +32,11 @@ const pageNav: React.FC<PageNavProps> = ({ name, position, title }) => {
             <ellipse cx="12" cy="17" rx="7" ry="4" fill="#C4C4C4" />
           </svg>
         </div>
-        <div className="flex flex-col text-right">
-          <span className="text-sm font-medium text-black leading-tight">
+        <div className="flex flex-col text-left">
+          <span className="text-lg font-semibold text-black leading-tight">
             {name}
           </span>
-          <span className="text-xs text-[#444]">{position}</span>
+          <span className="text-md text-[#444]">{position}</span>
         </div>
       </div>
     </nav>
